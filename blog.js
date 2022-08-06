6b2b2b82d588479e193b54ab89e961aa6fcae304
Will Nilges <wdn5796@rit.edu>
2022-08-06 01:18:12 -0400

    DUDE! HREF!

--- blog.js
+++ blog.js
@@ -1,3 +1,14 @@
+var posts = [
+    "2021-09-11_recovering_an_okd4_worker.md",
+    "2021-09-11_selinux_httpd.md",
+    "2021-09-05_Gitea_on_OKD4.md",
+    "2021-08-27_okd4_but_again.md",
+    "2021-08-13_restoring_old_project.md",
+    "2021-08-07_firewalld_services_blocker.md",
+    "2020-12-12_a_tldr_on_okd4.md",
+    "2020-05-17_subnetting_and_forgetting.md",
+];
+
 function getPost(post, target, legacy=false) {
     console.log(post);
     fetch('./posts/md/' + post)
@@ -29,16 +40,6 @@ function getPost(post, target, legacy=false) {
 }
 
 function loadPosts() {
-    let posts = [
-        "2021-09-11_recovering_an_okd4_worker.md",
-        "2021-09-11_selinux_httpd.md",
-        "2021-09-05_Gitea_on_OKD4.md",
-        "2021-08-27_okd4_but_again.md",
-        "2021-08-13_restoring_old_project.md",
-        "2021-08-07_firewalld_services_blocker.md",
-        "2020-12-12_a_tldr_on_okd4.md",
-        "2020-05-17_subnetting_and_forgetting.md",
-    ];
     let postsSpot = document.getElementById("blogJS");
 
     posts.forEach((post, index) => {
@@ -46,6 +47,17 @@ function loadPosts() {
     });
 }
 
+function generatePostLinks() {
+    let postDiv = document.getElementById("blogJS");
+    posts.forEach((post, index) => {
+        postDiv.innerHTML += `<a id="${post}" onclick="window.location.replace('post.html')">${post}</a><br>`;
+        
+    });
+}
+
 //getPost("test.md", document.getElementById("blogJS"));
 
-loadPosts();
+//loadPosts();
+
+
+generatePostLinks();
