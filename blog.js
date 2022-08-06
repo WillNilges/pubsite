87fb5c2462894ca45ba8b3fe914cdac5758eb9fe
Will Nilges <wdn5796@rit.edu>
2022-08-06 00:46:05 -0400

    postssss

--- blog.js
+++ blog.js
@@ -1,11 +1,8 @@
-function getPostMd(filename) {
-    let posts = [ "test.md" ];
-    let target = document.getElementById("blogJS");
-    posts.forEach((post, index) => {
-        console.log(post);
-        fetch('./posts/md/test.md')
-        .then(
-          function(response) {
+function getPost(post, target, legacy=false) {
+    console.log(post);
+    fetch('./posts/md/' + post)
+    .then(
+        function(response) {
             if (response.status !== 200) {
               console.log('Whoops! Can\'t get post. Status Code: ' +
                 response.status);
@@ -15,18 +12,20 @@ function getPostMd(filename) {
             // Examine the text in the response
            response.text().then(function(data) {
                 console.log(data);
-                target.innerHTML = markdown(data);
+                if (legacy) {
+                    target.innerHTML = data;
+                } else {
+                    target.innerHTML = markdown(data);
+                }
             }); 
-          }
-        )
-        .catch(function(err) {
-          console.log('Fetch Error :-S', err);
-        });
+        }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
     });
+    // posts.forEach((post, index) => {
+    //     
+    // });
 }
 
-function getPostHtml(filename) {
-    
-}
-
-getPostMd();
+getPost("test.md", document.getElementById("blogJS"));
