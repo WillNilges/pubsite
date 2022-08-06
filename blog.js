f3d686e3792cacff45cf45669f3588d029ca71d2
Will Nilges <wdn5796@rit.edu>
2022-08-06 01:03:15 -0400

    This kinda works.
    But it likes to switch the order around

--- blog.js
+++ blog.js
@@ -11,11 +11,11 @@ function getPost(post, target, legacy=false) {
 
             // Examine the text in the response
            response.text().then(function(data) {
-                console.log(data);
+                //console.log(data);
                 if (legacy) {
-                    target.innerHTML = data;
+                    target.innerHTML += data;
                 } else {
-                    target.innerHTML = markdown(data);
+                    target.innerHTML += markdown(data);
                 }
             }); 
         }
@@ -28,4 +28,24 @@ function getPost(post, target, legacy=false) {
     // });
 }
 
-getPost("test.md", document.getElementById("blogJS"));
+function loadPosts() {
+    let posts = [
+        "2021-09-11_recovering_an_okd4_worker.md",
+        "2021-09-11_selinux_httpd.md",
+        "2021-09-05_Gitea_on_OKD4.md",
+        "2021-08-27_okd4_but_again.md",
+        "2021-08-13_restoring_old_project.md",
+        "2021-08-07_firewalld_services_blocker.md",
+        "2020-12-12_a_tldr_on_okd4.md",
+        "2020-05-17_subnetting_and_forgetting.md",
+    ];
+    let postsSpot = document.getElementById("blogJS");
+
+    posts.forEach((post, index) => {
+        getPost(post, postsSpot);
+    });
+}
+
+//getPost("test.md", document.getElementById("blogJS"));
+
+loadPosts();
