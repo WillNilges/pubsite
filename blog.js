27fe8b44c680164f299b78ad0dd126bb9c148055
Will Nilges <wdn5796@rit.edu>
2022-08-08 20:16:53 -0400

    Move post titles and stuff to new file

--- blog.js
+++ blog.js
@@ -1,79 +1,3 @@
-const posts = [
-    "2021-09-15_selinux_httpd.md",
-    "2021-09-11_recovering_an_okd4_worker.md",
-    "2021-09-05_Gitea_on_OKD4.md",
-    "2021-08-27_okd4_but_again.md",
-    "2021-08-13_restoring_old_project.md",
-    "2021-08-07_firewalld_services_blocker.md",
-    "2020-12-12_a_tldr_on_okd4.md",
-    "2020-05-17_subnetting_and_forgetting.md",
-];
-
-// I'm not very good with computers.
-const titles = [
-  "Fixing some random 403's",
-  "Installing Gitea on OKD 4.7",
-  "Recovering an OKD 4 Worker",
-  "OKD 4, but again.",
-  "Troubles with `firewalld`",
-  "Re-Installing my First Major Project",
-  "A TL;DR on OKD 4",
-  "Subnetting and Forgetting",
-]
-
-//.... I'm REALLY not good with computers. These are old posts that are written in HTML and thus don't work with this system.
-// I've included them for posterity, but I'm not very proud of them.
-
-const legacyPosts = [
-    "2020-05-11_finishing_up.html",
-    "new_year.html",
-    "future_is_open.html",
-    "proper_i3_scaling.html",
-    "arch_scaling_hack.html",
-    "grub_arch_fix.html",
-    "seminar_mk_ii.html",
-    "windows_broke_time.html",
-    "graphics_output.html",
-    "no_automount.html",
-    "pimp_my_de.html",
-    "bumblebee_on_arch.html",
-    "networking_on_cli.html",
-];
-
-const legacyTitles = [
-    "Trying (and failing) to finish ShelfLife",
-    "A small update",
-    "The Future is Open reigstration happening now!",
-    "How to properly scale i3 on HiDPI displays",
-    "A neat HiDPI scaling hack",
-    "Installing GRUB on Arch",
-    "Intro to GNU/Linux (But mostly RHEL for RIT CE students) Mk II.",
-    "Windows broke my time!",
-    "Graphics output!",
-    "Thunar won't automount external drives",
-    "Pimp my DE seminar notes",
-    "How to install Bumblebee on Arch",
-    "How to set up networking on an uncooperative CLI",
-];
-
-const legacyDates = [
-  "2020-05-11",
-  "2020-01-13",
-  "2019-10-02",
-  "2019-08-13",
-  "2019-07-12",
-  "2019-03-13",
-  "2019-02-02",
-  "2019-02-04",
-  "2018-12-17",
-  "2018-11-27",
-  "2018-11-05",
-  "2018-10-29",
-  "2018-10-29",
-];
-
-//"embedded_nightmare.html",
-
 function getPost(post, legacy) {
     console.log(post);
     console.log(`legacy = ${legacy}`);
@@ -123,13 +47,13 @@ function generatePost() {
     }
 }
 
-function loadPosts() {
+/*function loadPosts() {
     let postsSpot = document.getElementById("blogJS");
 
     posts.forEach((post, index) => {
         getPost(post, postsSpot);
     });
-}
+}*/
 
 function generatePostLinks() {
     let postDiv = document.getElementById("main");
